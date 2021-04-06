54cf24119b54bbf32f74839792a8125ba5dff298
HANJIYEONN <onedelay1@naver.com>
2021-04-06 21:39:17 +0900

    Initial commit

---
+++ src/main/java/hello/hellosprings/HelloSpringsApplication.java
@@ -0,0 +1,13 @@
+package hello.hellosprings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSpringsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloSpringsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hello/hellosprings/HelloSpringsApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.hellosprings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSpringsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
